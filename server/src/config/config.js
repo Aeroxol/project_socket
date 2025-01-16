59b134e68675bdf438790778857055ff122c8fbd
ChanilPark <49317214+Aeroxol@users.noreply.github.com>
2025-01-16 15:33:17 +0900

    db 연동하기
    로컬 db에 연결하고 테스트하기

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,5 +1,12 @@
 import { PORT, HOST, CLIENT_VERSION } from '../constants/env.js';
 import { PACKET_TYPE_LENGTH, TOTAL_LENGTH } from '../constants/header.js';
+import {
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+} from '../constants/env.js';
 
 export const config = {
   server: {
@@ -12,6 +19,16 @@ export const config = {
   packet: {
     totalLength: TOTAL_LENGTH,
     typeLength: PACKET_TYPE_LENGTH,
+  },
+  databases: {
+    USER_DB: {
+      name: DB_NAME,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      host: DB_HOST,
+      port: DB_PORT,
+    },
+    // 필요한 만큼 추가
   },
   // 필요한 만큼 추가
 };
